aa75e3c9c81159b7ed5a39382a9dd0cae0c50b75
linden8987 <lindenjiganie123@icloud.com>
2026-04-28 07:57:24 -0700

    Update install script to download ZIP and 7zip
    I added 7zip so that you could actually open the zip file because you're in recovery service so how could you?

--- install.sh
+++ install.sh
@@ -11,24 +11,27 @@ ARCH=$(uname -m)
 echo "System detected: $ARCH"
 
 # 2. Set the Download URL
-# This points to the latest EXE built by your GitHub Actions
+# This points to the latest ZIP built by your GitHub Actions
 REPO_URL="https://github.com/linden8987/RecoveryMore/releases/latest/download/RecoveryMore.zip"
+ZIP="https://github.com/ip7z/7zip/releases/download/26.01/7z2601-x64.exe"
 
 # 3. Create a temporary workspace in RAM
 mkdir -p /tmp/RecoveryMore
 cd /tmp/RecoveryMore
 
 # 4. Download the "Graphical Guy"
-echo "Fetching RecoveryMore GUI from GitHub..."
-curl -L $REPO_URL -o RecoveryMore.exe
+echo "Fetching RecoveryMore ZIP from GitHub..."
+curl -L $REPO_URL -o RecoveryMore.zip
+curl -L $ZIP -o 7zip.exe
 
 if [ $? -eq 0 ]; then
     echo "Download Complete."
     echo "Launching Graphical Interface..."
     
-    # 5. Launch the EXE
+    # 5. Launch 7zip and RecoveryMore
     # In WinRE, we can execute the EXE directly from the shell
-    ./RecoveryMore.exe
+    ./7zip.exe
+    ./RecoveryMore.zip
 else
     echo "Error: Could not reach GitHub. Check your internet connection."
 fi
